ce8437a555d84d3f4108fe5eda043464d69f28a2
anucha-tk <anuchataowkaen@gmail.com>
2025-02-18 16:18:49 +0700

    feat(layout): add menu and add field on estate model

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -4,9 +4,12 @@
     "description": """""",
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
     "installable": True,
     "application": True,
     "auto_install": False,
     "license": "LGPL-3",
+    "sequence": -100,
 }  # type: ignore
--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,3 +1,4 @@
+from dateutil.relativedelta import relativedelta
 from odoo import fields, models
 
 
@@ -19,15 +20,24 @@ class EstateProperty(models.Model):
         ),
     ]
 
-    # Basic
+    # Default Method
+    def _default_date_availability(self):
+        return fields.Date.context_today(self) + relativedelta(month=3)
+
+    # Field Declaration
+    ## Basic
     name = fields.Char("Name", required=True)
     description = fields.Text("Description")
     postcode = fields.Char("Postcode")
-    date_availability = fields.Date("Date Availability")
+    date_availability = fields.Date(
+        "Date Availability",
+        default=lambda self: self._default_date_availability(),
+        copy=False,
+    )
     expected_price = fields.Float("Expected Price", required=True)
-    selling_price = fields.Float("Selling Price")
-    bedrooms = fields.Integer("Bedroom")
-    living_area = fields.Integer("Living Area")
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer("Bedroom", default=2)
+    living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer("Facades")
     garage = fields.Boolean("Garage")
     garden = fields.Boolean("Garden")
@@ -40,4 +50,20 @@ class EstateProperty(models.Model):
             ("E", "East"),
             ("W", "West"),
         ],
+    )
+    active = fields.Boolean("Active", default=True)
+
+    # Special
+    state = fields.Selection(
+        string="Status",
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
     )
