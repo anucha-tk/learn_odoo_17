d3dd3583031b78a3cf45c429c547d25e70c9b980
anucha-tk <anuchataowkaen@gmail.com>
2025-03-03 20:59:25 +0700

    make init and manifest

---
+++ addons/student/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "student",
+    "summary": "Short (1 phrase/line) summary of the module's purpose",
+    "description": """
+Long description of module's purpose
+    """,
+    "author": "Anucha-tk",
+    "website": "https://www.yourcompany.com",
+    "category": "Uncategorized",
+    "version": "0.1",
+    "depends": ["base", "account"],
+    "data": [],
+}
