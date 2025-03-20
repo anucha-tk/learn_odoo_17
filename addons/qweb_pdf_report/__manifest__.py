ec9a4f66e578074bb6a9fd606cbc767abc6925ae
anucha-tk <anuchataowkaen@gmail.com>
2025-03-20 21:28:47 +0700

    feat: add report student

---
+++ addons/qweb_pdf_report/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "qweb_pdf_report",
+    "summary": "Short (1 phrase/line) summary of the module's purpose",
+    "description": """
+Long description of module's purpose
+    """,
+    "author": "My Company",
+    "website": "https://www.yourcompany.com",
+    "category": "Uncategorized",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [
+        "report/student_report_template.xml",
+    ],
+}
